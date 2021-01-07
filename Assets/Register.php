24dbcccc7e1d499dac5ad8a9f433d9d8d1730d20
Yves Huber <yves.huber05@gmail.com>
2021-01-07 07:26:18 +0100

    tried to add the markers on the map

--- Assets/Register.php
+++ Assets/Register.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password ="";
+$password = "";
 $dbname = "happyplace";
 
 // Create connection
@@ -10,15 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
-$sql = "SELECT name FROM places";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo "OWO: " . $row["name"];
-    }
-  } else {
-    echo "0 results";
-  }
 $conn->close();
+
+
+?>
--- index.php
+++ index.php
@@ -7,58 +7,87 @@ $dbname = "happyplace";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "SELECT name FROM places";
+$lat = "SELECT latitude, longitude FROM places";
 $result = $conn->query($sql);
+$resultla = $conn->query($lat);
 
 // create curl resource
 $ch = curl_init();
 
 //return the transfer as a string
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
+start:
 // $output contains the output string
 if ($result->num_rows > 0) {
-
     // output data of each row
     while ($row = $result->fetch_assoc()) {
-
         echo " Ort: " . $row["name"];
         echo " ";
-        curl_setopt($ch, CURLOPT_URL, "https://nominatim.openstreetmap.org/search?q=" . $row["name"] . "&format=json");
-        $output = curl_exec($ch);
         printf($output);
     }
 } else {
     echo "0 results";
 }
+
+if ($resultla->num_rows > 0) {
+    while ($row = $resultla->fetch_assoc()) {
+        echo " lat: " . $row["latitude"] . " long: " . $row["longitude"];
+        $a = $row["latitude"];
+        $b = $row["longitude"];
+        printf($output);
+    }
+} else {
+    echo "0 results";
+}
+
 echo curl_error($ch);
 
-
 // close curl resource to free up system resources
-curl_close($ch);
-$conn->close();
+
 ?>
 <!DOCTYPE HTML>
 <html>
 
 <head>
     <title>OpenLayers Demo</title>
+    <link rel="stylesheet" href="/Assets/style.css">
     <style type="text/css">
         html,
         body,
         #basicMap {
-            width: 100%;
-            height: 100%;
+            width: 75%;
+            height: 75%;
             margin: 0;
         }
     </style>
-    <script src="OpenLayers.js"></script>
     <script>
+            function add_map_point(lng, lat) {
+            var vectorLayer = new ol.layer.Vector({
+                source: new ol.source.Vector({
+                    features: [new ol.Feature({
+                        geometry: new ol.geom.Point(ol.proj.transform([parseFloat(lng), parseFloat(lat)], 'EPSG:4326', 'EPSG:3857')),
+                    })]
+                }),
+                style: new ol.style.Style({
+                    image: new ol.style.Icon({
+                        anchor: [0.5, 0.5],
+                        anchorXUnits: "fraction",
+                        anchorYUnits: "fraction",
+                        src: "https://upload.wikimedia.org/wikipedia/commons/e/ec/RedDot.svg"
+                    })
+                })
+            });
+            map.addLayer(vectorLayer);
+            console.log(lat);
+        }
+
         function init() {
             map = new OpenLayers.Map("basicMap");
             var mapnik = new OpenLayers.Layer.OSM();
@@ -70,11 +99,18 @@ $conn->close();
             map.addLayer(mapnik);
             map.setCenter(position, zoom);
         }
+        </script>
+    <script src="OpenLayers.js"></script>
+    <script>
+        var lat2 = <?php echo $a; ?>;
+        var lng2 = <?php echo $b; ?>;
+        console.log (lat2);
+        console.log (lng2);
     </script>
 </head>
 
-<body onload="init();">
+<body onload="init(); add_map_point(lat2, lng2);">
     <div id="basicMap"></div>
-</body>
+    </body>
 
 </html>
