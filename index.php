8695be0bf5561b05eceeb054cf52cb5a12ffcc34
Yves Huber <yves.huber05@gmail.com>
2021-01-11 13:12:05 +0100

    Marker and Map working

--- index.php
+++ index.php
@@ -1,3 +1,51 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>OpenLayers Demo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Assets/style.css">
+    <style type="text/css">
+        html,
+        body,
+        #basicMap {
+            width: 75%;
+            height: 75%;
+            margin: 0;
+        }
+    </style>
+    <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/build/ol.js"></script>
+    <script>
+        function add_map_point(lng, lat) {
+            var layer = new ol.layer.Vector({
+                source: new ol.source.Vector({
+                    features: [
+                        new ol.Feature({
+                            geometry: new ol.geom.Point(ol.proj.fromLonLat([lng, lat]))
+                        })
+                    ]
+                })
+            });
+            map.addLayer(layer);
+        }
+
+
+        function init() {
+            return new ol.Map({
+                target: 'basicMap',
+                layers: [
+                    new ol.layer.Tile({
+                        source: new ol.source.OSM()
+                    })
+                ],
+                view: new ol.View({
+                    center: ol.proj.fromLonLat([8.510, 47.36]),
+                    zoom: 4
+                })
+            });
+        }
+    </script>
 <?php
 ini_set('user_agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36');
 $servername = "localhost";
@@ -15,16 +63,12 @@ if ($conn->connect_error) {
 
 $sql = "SELECT name FROM places";
 $lat = "SELECT latitude, longitude FROM places";
+$url = "SELECT icon FROM markers";
 $result = $conn->query($sql);
 $resultla = $conn->query($lat);
+$resulturl = $conn->query($url);
 
-// create curl resource
-$ch = curl_init();
 
-//return the transfer as a string
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-start:
-// $output contains the output string
 if ($result->num_rows > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
@@ -37,8 +81,8 @@ if ($result->num_rows > 0) {
 }
 
 if ($resultla->num_rows > 0) {
-    while ($row = $resultla->fetch_assoc()) {
-        echo " lat: " . $row["latitude"] . " long: " . $row["longitude"];
+    if ($row = $resultla->fetch_assoc()) {
+        echo " lat: " . $row["latitude"]  . " long: " . $row["longitude"];
         $a = $row["latitude"];
         $b = $row["longitude"];
         printf($output);
@@ -47,70 +91,24 @@ if ($resultla->num_rows > 0) {
     echo "0 results";
 }
 
-echo curl_error($ch);
 
 // close curl resource to free up system resources
 
 ?>
-<!DOCTYPE HTML>
-<html>
-
-<head>
-    <title>OpenLayers Demo</title>
-    <link rel="stylesheet" href="/Assets/style.css">
-    <style type="text/css">
-        html,
-        body,
-        #basicMap {
-            width: 75%;
-            height: 75%;
-            margin: 0;
-        }
-    </style>
-    <script>
-            function add_map_point(lng, lat) {
-            var vectorLayer = new ol.layer.Vector({
-                source: new ol.source.Vector({
-                    features: [new ol.Feature({
-                        geometry: new ol.geom.Point(ol.proj.transform([parseFloat(lng), parseFloat(lat)], 'EPSG:4326', 'EPSG:3857')),
-                    })]
-                }),
-                style: new ol.style.Style({
-                    image: new ol.style.Icon({
-                        anchor: [0.5, 0.5],
-                        anchorXUnits: "fraction",
-                        anchorYUnits: "fraction",
-                        src: "https://upload.wikimedia.org/wikipedia/commons/e/ec/RedDot.svg"
-                    })
-                })
-            });
-            map.addLayer(vectorLayer);
-            console.log(lat);
-        }
-
-        function init() {
-            map = new OpenLayers.Map("basicMap");
-            var mapnik = new OpenLayers.Layer.OSM();
-            var fromProjection = new OpenLayers.Projection("EPSG:4326"); // Transform from WGS 1984
-            var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-            var position = new OpenLayers.LonLat(8.52, 47.36).transform(fromProjection, toProjection);
-            var zoom = 10;
-
-            map.addLayer(mapnik);
-            map.setCenter(position, zoom);
-        }
-        </script>
-    <script src="OpenLayers.js"></script>
-    <script>
-        var lat2 = <?php echo $a; ?>;
-        var lng2 = <?php echo $b; ?>;
-        console.log (lat2);
-        console.log (lng2);
-    </script>
 </head>
 
-<body onload="init(); add_map_point(lat2, lng2);">
+<body>
     <div id="basicMap"></div>
-    </body>
+    <script type="text/javascript">
+        var map = init();
+        lat2 = <?php echo $a; ?>;
+        lng2 = <?php echo $b; ?>;
+        
+        add_map_point(lng2, lat2);
+    </script>
+
+
+    </div>
+</body>
 
 </html>
